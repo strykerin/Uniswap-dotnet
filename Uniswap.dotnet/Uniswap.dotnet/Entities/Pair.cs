de666e2a7971880eb5839a471900739850b1c8d9
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 22:59:49 -0300

    Create Pair.cs

---
+++ Uniswap.dotnet/Uniswap.dotnet/Entities/Pair.cs
@@ -0,0 +1,10 @@
+﻿namespace Uniswap.dotnet.Entities
+{
+    public class Pair
+    {
+        public string ReserveETH { get; set; }
+        public string ReserveUSD { get; set; }
+        public Token Token0 { get; set; }
+        public Token Token1 { get; set; }
+    }
+}
