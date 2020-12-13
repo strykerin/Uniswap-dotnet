4e8990ca4c7d789140ef68bea79d275b2ee7a81d
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 23:01:06 -0300

    GetMostLiquidMarketPairs

---
+++ Uniswap.dotnet/Uniswap.dotnet/IUniswap.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Uniswap.dotnet.Entities;
+
+namespace Uniswap.dotnet
+{
+    public interface IUniswap
+    {
+        Task<Pools> GetMostLiquidMarketPairs();
+    }
+}
--- Uniswap.dotnet/Uniswap.dotnet/Uniswap.cs
+++ Uniswap.dotnet/Uniswap.dotnet/Uniswap.cs
@@ -1,9 +1,42 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using System;
+using GraphQL;
+using Uniswap.dotnet.Entities;
+using GraphQL.Client.Abstractions;
 
 namespace Uniswap.dotnet
 {
-    public class Uniswap
+    public class Uniswap : IUniswap
     {
-        private readonly 
+        private readonly IGraphQLClient _graphQLClient;
+
+        public Uniswap(IGraphQLClient graphQLHttpClient)
+        {
+            _graphQLClient = graphQLHttpClient ?? throw new ArgumentNullException(nameof(graphQLHttpClient));
+        }
+
+        public async Task<Pools> GetMostLiquidMarketPairs()
+        {
+            var query = new GraphQLRequest
+            {
+                Query = @"
+                    {
+                      pairs {
+                        token0 {
+                          symbol
+                        }
+                        token1 {
+                          symbol
+                        }
+                        reserveETH
+                        reserveUSD
+                      }
+                    }
+                "
+            };
+
+            var response = await _graphQLClient.SendQueryAsync<Pools>(query);
+            return response.Data;
+        }
     }
 }
