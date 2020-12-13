74907bbe2b4c9cec2931506c835b5f48fd5916ea
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 23:00:18 -0300

    Create Uniswap Extension method

---
+++ Uniswap.dotnet/Uniswap.dotnet/Extensions/UniswapExtension.cs
@@ -0,0 +1,29 @@
+﻿using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Uniswap.dotnet.Extensions
+{
+    public static class UniswapExtension
+    {
+        public static void AddUniswap(this IServiceCollection services)
+        {
+            services.AddSingleton<IGraphQLClient>(ctx =>
+            {
+                var graphQLOptions = new GraphQLHttpClientOptions
+                {
+                    EndPoint = new Uri("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
+                };
+                return new GraphQLHttpClient(graphQLOptions, new SystemTextJsonSerializer());
+            });
+
+            services.AddSingleton<IUniswap>(ctx => 
+            {
+                IGraphQLClient graphQLClient = ctx.GetRequiredService<IGraphQLClient>();
+                return new Uniswap(graphQLClient);
+            });
+        }
+    }
+}
