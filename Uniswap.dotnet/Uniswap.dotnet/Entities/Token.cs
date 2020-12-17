d26564568c6b012e3c952b5087502df5abc9679e
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-16 23:15:54 -0300

    Add more properties in Token class

--- Uniswap.dotnet/Uniswap.dotnet/Entities/Token.cs
+++ Uniswap.dotnet/Uniswap.dotnet/Entities/Token.cs
@@ -1,7 +1,25 @@
-﻿namespace Uniswap.dotnet.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Uniswap.dotnet.Entities
 {
     public class Token
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string Symbol { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string TradeVolume { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string TradeVolumeUSD { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string TotalSupply { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string TotalLiquidity { get; set; }
     }
 }
